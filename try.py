c7937b8e75f8ce7c2f6bd1937f0a186937471b75
Piyush_07 <luitelpiyush95@gmail.com>
2023-11-20 09:05:13 +0545

    important stats added to front-end

--- try.py
+++ try.py
@@ -40,3 +40,26 @@ if user_menu == 'Overall Analysis':
     events = df['Event'].unique().shape[0]
     athletes = df['Name'].unique().shape[0]
     nations = df['region'].unique().shape[0]
+
+    st.title("Top Statistics")
+    col1,col2,col3 = st.beta_columns(3)
+    with col1:
+        st.header("Editions")
+        st.title(editions)
+    with col2:
+        st.header("Hosts")
+        st.title(cities)
+    with col3:
+        st.header("Sports")
+        st.title(sports)
+
+    col1, col2, col3 = st.beta_columns(3)
+    with col1:
+        st.header("Events")
+        st.title(events)
+    with col2:
+        st.header("Nations")
+        st.title(nations)
+    with col3:
+        st.header("Athletes")
+        st.title(athletes)
