d6a462c29848d84b0bafe301a00d3ef0d9dd8f84
Piyush_07 <luitelpiyush95@gmail.com>
2023-11-20 06:44:54 +0545

    image added in sidebar

--- try.py
+++ try.py
@@ -2,3 +2,4 @@ import streamlit as st
 import pandas as pd
 
 st.sidebar.title("Olympics Analysis")
+st.sidebar.image('https://e7.pngegg.com/pngimages/1020/402/png-clipart-2024-summer-olympics-brand-circle-area-olympic-rings-olympics-logo-text-sport.png')
