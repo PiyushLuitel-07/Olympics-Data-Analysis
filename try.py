63c2d9f73250f51b1fa4443c8fd873d5e5608a87
Piyush_07 <luitelpiyush95@gmail.com>
2023-11-20 08:03:43 +0545

    csv files is read

--- try.py
+++ try.py
@@ -1,8 +1,15 @@
 import streamlit as st
 import pandas as pd
+import preprocessor,helper
+
+df = pd.read_csv('athlete_events.csv')
+region_df = pd.read_csv('noc_regions.csv')
+
+df = preprocessor.preprocess(df,region_df)
 
 st.sidebar.title("Olympics Analysis")
 st.sidebar.image('https://e7.pngegg.com/pngimages/1020/402/png-clipart-2024-summer-olympics-brand-circle-area-olympic-rings-olympics-logo-text-sport.png')
+
 user_menu = st.sidebar.radio(
     'Select an Option',
     ('Medal Tally','Overall Analysis','Country-wise Analysis','Athlete wise Analysis')
