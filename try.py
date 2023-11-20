142bf320adb77f7ed7e967c179b1e8bc8890218d
Piyush_07 <luitelpiyush95@gmail.com>
2023-11-20 06:46:29 +0545

    radiobuttons added in sidebar

--- try.py
+++ try.py
@@ -3,3 +3,7 @@ import pandas as pd
 
 st.sidebar.title("Olympics Analysis")
 st.sidebar.image('https://e7.pngegg.com/pngimages/1020/402/png-clipart-2024-summer-olympics-brand-circle-area-olympic-rings-olympics-logo-text-sport.png')
+user_menu = st.sidebar.radio(
+    'Select an Option',
+    ('Medal Tally','Overall Analysis','Country-wise Analysis','Athlete wise Analysis')
+)
