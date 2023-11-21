6cbd4d85921bc57be67573b60f5a73268828f7cc
Piyush_07 <luitelpiyush95@gmail.com>
2023-11-21 12:29:20 +0545

    famous_sport list added

--- try.py
+++ try.py
@@ -133,3 +133,15 @@ if user_menu == 'Athlete wise Analysis':
     fig.update_layout(autosize=False,width=1000,height=600)
     st.title("Distribution of Age")
     st.plotly_chart(fig)
+
+    x = []
+    name = []
+    famous_sports = ['Basketball', 'Judo', 'Football', 'Tug-Of-War', 'Athletics',
+                     'Swimming', 'Badminton', 'Sailing', 'Gymnastics',
+                     'Art Competitions', 'Handball', 'Weightlifting', 'Wrestling',
+                     'Water Polo', 'Hockey', 'Rowing', 'Fencing',
+                     'Shooting', 'Boxing', 'Taekwondo', 'Cycling', 'Diving', 'Canoeing',
+                     'Tennis', 'Golf', 'Softball', 'Archery',
+                     'Volleyball', 'Synchronized Swimming', 'Table Tennis', 'Baseball',
+                     'Rhythmic Gymnastics', 'Rugby Sevens',
+                     'Beach Volleyball', 'Triathlon', 'Rugby', 'Polo', 'Ice Hockey']
