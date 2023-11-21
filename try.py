bf92d2ab6742e63ab18efc12e6b297c2ebbd5319
Piyush_07 <luitelpiyush95@gmail.com>
2023-11-21 11:55:18 +0545

    some changes done

--- try.py
+++ try.py
@@ -95,6 +95,7 @@ if user_menu == 'Overall Analysis':
     sport_list.sort()
     sport_list.insert(0,'Overall')
     selected_sport = st.selectbox('Select a Sport',sport_list)
+    st.title("15 most successful atheles in "+selected_sport)
     x = helper.most_successful(df,selected_sport)
     st.table(x)
 
