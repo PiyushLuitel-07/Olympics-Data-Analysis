5d3f32cb8b49ea63a2c8c0db7bfde49735b7593d
Piyush_07 <luitelpiyush95@gmail.com>
2023-11-21 12:26:26 +0545

    plot done

--- try.py
+++ try.py
@@ -129,3 +129,7 @@ if user_menu == 'Athlete wise Analysis':
     x3 = athlete_df[athlete_df['Medal'] == 'Silver']['Age'].dropna()
     x4 = athlete_df[athlete_df['Medal'] == 'Bronze']['Age'].dropna()
 
+    fig = ff.create_distplot([x1, x2, x3, x4], ['Overall Age', 'Gold Medalist', 'Silver Medalist', 'Bronze Medalist'],show_hist=False, show_rug=False)
+    fig.update_layout(autosize=False,width=1000,height=600)
+    st.title("Distribution of Age")
+    st.plotly_chart(fig)
